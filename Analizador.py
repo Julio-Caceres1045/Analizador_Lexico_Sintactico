a5cb818d462e4ecad9f2eea74717154bc9a00fe4
Julio Cáceres <125315663+Julio-Caceres1045@users.noreply.github.com>
2025-03-24 11:42:38 -0600

    Update Analizador.py

--- Analizador.py
+++ Analizador.py
@@ -36,6 +36,10 @@ class NodoAst:
                 result[key] = value
         return result
 
+class NodoPrograma(NodoAst):
+    def __init__(self, funciones):
+        self.funciones = funciones
+
 class NodoFuncion(NodoAst):
     def __init__(self, tipo, nombre, parametros, cuerpo):
         self.tipo = tipo
@@ -50,6 +54,12 @@ class NodoParametro(NodoAst):
 
 class NodoAsignacion(NodoAst):
     def __init__(self, nombre, expresion):
+        self.nombre = nombre
+        self.expresion = expresion
+
+class NodoDeclaracion(NodoAst):
+    def __init__(self, tipo, nombre, expresion):
+        self.tipo = tipo
         self.nombre = nombre
         self.expresion = expresion
 
@@ -87,7 +97,10 @@ class Parser:
         raise SyntaxError(f'Error Sintáctico, se esperaba {tipo_esperado}, pero se encontró {simbolo_actual}')
 
     def parsear(self):
-        return self.funcion()
+        funciones = []
+        while self.obtener_simbolo() and self.obtener_simbolo()[0] == "KEYWORD":
+            funciones.append(self.funcion())
+        return NodoPrograma(funciones)
 
     def funcion(self):
         tipo = self.aceptar_token("KEYWORD")[1]
@@ -95,14 +108,19 @@ class Parser:
         self.aceptar_token("DELIMITER")
         parametros = self.parametros()
         self.aceptar_token("DELIMITER")
+        
+        if self.obtener_simbolo() and self.obtener_simbolo()[1] == ";":
+            self.aceptar_token("DELIMITER")
+            return NodoFuncion(tipo, nombre, parametros, [])
+        
         self.aceptar_token("DELIMITER")
         cuerpo = self.cuerpo()
         self.aceptar_token("DELIMITER")
         return NodoFuncion(tipo, nombre, parametros, cuerpo)
-
+    
     def parametros(self):
         parametros = []
-        if self.obtener_simbolo() and self.obtener_simbolo()[0] == "KEYWORD":
+        while self.obtener_simbolo() and self.obtener_simbolo()[0] == "KEYWORD":
             tipo = self.aceptar_token("KEYWORD")[1]
             nombre = self.aceptar_token("IDENTIFIER")[1]
             parametros.append(NodoParametro(tipo, nombre))
@@ -118,24 +136,31 @@ class Parser:
         while self.obtener_simbolo() and self.obtener_simbolo()[1] != "}":
             simbolo_actual = self.obtener_simbolo()
             if simbolo_actual[0] == "KEYWORD":
-                if simbolo_actual[1] == "return":
-                    self.aceptar_token("KEYWORD")
-                    expresion = self.evaluar_expresion()
-                    self.aceptar_token("DELIMITER")
-                    cuerpo.append(NodoRetorno(expresion))
-                else:
-                    cuerpo.append(self.asignacion())
-            else:
+                cuerpo.append(self.declaracion())
+            elif simbolo_actual[0] == "IDENTIFIER":
                 cuerpo.append(self.asignacion())
+            elif simbolo_actual[0] == "NUMBER":
+                cuerpo.append(NodoNumero(self.aceptar_token("NUMBER")[1]))
+            elif simbolo_actual[0] == "DELIMITER" and simbolo_actual[1] == ",":
+                self.aceptar_token("DELIMITER")
+                continue
+            elif simbolo_actual[0] == "KEYWORD" and simbolo_actual[1] == "return":
+                self.aceptar_token("KEYWORD")
+                expresion = self.evaluar_expresion()
+                self.aceptar_token("DELIMITER")
+                cuerpo.append(NodoRetorno(expresion))
+            else:
+                raise SyntaxError(f"Error inesperado en el cuerpo: {simbolo_actual}")
         return cuerpo
 
-    def asignacion(self):
+    def declaracion(self):
+        tipo = self.aceptar_token("KEYWORD")[1]
         nombre = self.aceptar_token("IDENTIFIER")[1]
         self.aceptar_token("OPERATOR")
         expresion = self.evaluar_expresion()
         self.aceptar_token("DELIMITER")
-        return NodoAsignacion(nombre, expresion)
-
+        return NodoDeclaracion(tipo, nombre, expresion)
+    
     def evaluar_expresion(self):
         izquierda = self.evaluar_componente()
         while self.obtener_simbolo() and self.obtener_simbolo()[0] == "OPERATOR":
@@ -143,7 +168,7 @@ class Parser:
             derecha = self.evaluar_componente()
             izquierda = NodoOperacion(izquierda, operador, derecha)
         return izquierda
-
+    
     def evaluar_componente(self):
         simbolo_actual = self.obtener_simbolo()
         if simbolo_actual[0] == "NUMBER":
@@ -151,6 +176,66 @@ class Parser:
         elif simbolo_actual[0] == "IDENTIFIER":
             return NodoIdentificador(self.aceptar_token("IDENTIFIER")[1])
         raise SyntaxError(f"Error al analizar el componente: {simbolo_actual}")
+    
+    # Traducción de AST a Ensamblador
+class GeneradorASM:
+    def traducir_if(self, nodo):
+        asm = "".join([
+            f"CMP {nodo.condicion}, 1\n",
+            "JNE ELSE_BLOCK\n",
+            "; Código dentro del if\n",
+            "JMP END_IF\n",
+            "ELSE_BLOCK:\n" if nodo.cuerpo_else else "",
+            "; Código dentro del else\n" if nodo.cuerpo_else else "",
+            "END_IF:\n"
+        ])
+        return asm
+
+    def traducir_while(self, nodo):
+        asm = "".join([
+            "WHILE_START:\n",
+            f"CMP {nodo.condicion}, 0\n",
+            "JLE WHILE_END\n",
+            "; Código dentro del while\n",
+            "JMP WHILE_START\n",
+            "WHILE_END:\n"
+        ])
+        return asm
+
+    def traducir_for(self, nodo):
+        asm = "".join([
+            f"MOV CX, {nodo.inicializacion}\n",
+            "FOR_START:\n",
+            f"CMP CX, {nodo.condicion}\n",
+            "JGE FOR_END\n",
+            "; Código dentro del for\n",
+            "INC CX\n",
+            "JMP FOR_START\n",
+            "FOR_END:\n"
+        ])
+        return asm
+
+    def traducir_print(self, nodo):
+        asm = "".join([
+            f"MOV DX, OFFSET {nodo.expresion}\n",
+            "MOV AH, 09h\n",
+            "INT 21h\n"
+        ])
+        return asm
+
+    def generar_asm(self, ast):
+        codigo_asm = ""  
+        if isinstance(ast, NodoIf):
+            codigo_asm += self.traducir_if(ast)
+        elif isinstance(ast, NodoWhile):
+            codigo_asm += self.traducir_while(ast)
+        elif isinstance(ast, NodoFor):
+            codigo_asm += self.traducir_for(ast)
+        elif isinstance(ast, NodoPrint):
+            codigo_asm += self.traducir_print(ast)
+        return codigo_asm
+
+    
 
 # Función para exportar el AST a un archivo JSON
 def exportar_ast(ast, filename="ast.json"):
