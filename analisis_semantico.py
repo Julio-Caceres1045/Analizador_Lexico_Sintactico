412a3796b2be5181f76229f02168ff5484a5bf0f
Julio Cáceres <125315663+Julio-Caceres1045@users.noreply.github.com>
2025-05-14 11:36:34 -0600

    Update analisis_semantico.py
    Corrección de errores lexicos que afectaban la correcta ejecución del programa

--- analisis_semantico.py
+++ analisis_semantico.py
@@ -12,19 +12,17 @@ class AnalizadorSemantico:
 
             if token[0] == "KEYWORD":
                 if token[1] == "int" or token[1] == "float":
-                    # Declaración de variable
                     tipo = token[1]
-                    i += 1  # Avanzamos para obtener el identificador
+                    i += 1  
                     if tokens[i][0] == "IDENTIFIER":
                         var_name = tokens[i][1]
                         if var_name in self.variables:
                             raise Exception(f"Error: Variable '{var_name}' ya declarada.")
                         self.variables[var_name] = tipo
-                    i += 1  # Avanzamos al siguiente token
+                    i += 1  
 
                 elif token[1] == "return":
-                    # Verificar retorno (se asume que el valor debe ser de tipo correcto)
-                    i += 1  # Avanzamos para obtener el valor del retorno
+                    i += 1 
                     if tokens[i][0] == "IDENTIFIER":
                         var_name = tokens[i][1]
                         if var_name not in self.variables:
@@ -32,23 +30,19 @@ class AnalizadorSemantico:
                     i += 1
 
                 elif token[1] == "void":
-                    # Declaración de función (por ejemplo, main())
-                    i += 1  # Avanzamos para obtener el nombre de la función
+                    i += 1  
                     if tokens[i][0] == "IDENTIFIER":
                         func_name = tokens[i][1]
                         if func_name in self.funciones:
-                            raise Exception(f"Error: Función '{func_name}' ya declarada.")
+                            raise Exception(f"Error: Funcion '{func_name}' ya declarada.")
                         self.funciones[func_name] = "void"
                     i += 1
 
                 elif token[1] == "print":
-                    # Verificar que 'print' tiene un argumento válido
                     i += 1
                     if tokens[i][0] == "IDENTIFIER":
                         var_name = tokens[i][1]
                         if var_name not in self.variables:
                             raise Exception(f"Error: Variable '{var_name}' no declarada en la llamada a 'print'.")
                     i += 1
-
-            # Procesamos operadores y otros elementos según sea necesario
             i += 1
